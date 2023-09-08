b1d38ddc2b53c3bed9cbbbc0a0a6f199026b8b76
ThaLonga <simondelange2@gmail.com>
2023-09-08 17:44:58 +0200

    modelling

---
+++ modelling.R
@@ -0,0 +1,60 @@
+#########################
+#####Logistic regression
+#########################
+
+if (!require("pacman")) install.packages("pacman") ; require("pacman")
+p_load(glmnet, tidyverse)
+
+## Ridge Regression to create the Adaptive Weights Vector
+set.seed(123)
+cv.ridge <- cv.glmnet(x, y, family='binomial', alpha=0, parallel=TRUE, standardize=TRUE)
+
+# weights = 1/absolute value of ridge coefficients
+w3 <- 1/abs(matrix(coef(cv.ridge, s=cv.ridge$lambda.min)
+                   [, 1][2:(ncol(x)+1)] ))^1 ## Using gamma = 1
+w3[w3[,1] == Inf] <- 999999999 ## Replacing values estimated as Infinite for 999999999
+
+# adaptive Lasso
+set.seed(123)
+cv.lasso <- cv.glmnet(x, y, family='binomial', alpha=1, parallel=TRUE, standardize=TRUE, type.measure='auc', penalty.factor=w3)
+
+#####
+# evaluation
+#####
+
+#based on Lessmann et al. 3 performance measures
+
+#AUC
+xpreds <- gold
+prob=predict(cv.lasso, newx = x,type=c("response"))
+xpreds$prob=prob
+library(pROC)
+g <- roc(good ~ prob, data = xpreds)
+plot(g)    
+AUC <- g$auc
+#0.7973
+
+#PG
+
+#BS
+#accuracy: closer to 0 = better (1/N)*sum((f-o)²)
+
+(BS <- sum((prob-y)^2)/nrow(y))
+#0.1617
+
+# plots
+plot(cv.lasso)
+plot(cv.lasso$glmnet.fit, xvar="lambda", label=TRUE)
+abline(v = log(cv.lasso$lambda.min))
+abline(v = log(cv.lasso$lambda.1se))
+coef(cv.lasso, s=cv.lasso$lambda.1se)
+coef <- coef(cv.lasso, s='lambda.1se')
+selected_attributes <- (coef@i[-1]+1) ## Considering the structure of the data frame dataF as shown earlier
+
+#####Rule ensembles
+
+#adaptive lasso
+
+#####SRE
+
+#adaptive lasso
--- preprocessing.R
+++ preprocessing.R
@@ -1,9 +1,33 @@
-library(readr)
-german <- read_table("data/statlog+german+credit+data/german.csv", 
-                       +     col_names = FALSE)
+if (!require("pacman")) install.packages("pacman") ; require("pacman")
+p_load(tidyverse)
 
-names(german) <- c("Status of existing checking account", "Duration in month", "Credit history", "Purpose", "Credit amount",
-                   "Savings account/bonds", "Present employment since", "Installment rate in percentage of disposable income",
-                   "Personal status and sex", "Other debtors / guarantors", "Present residence since", "Present residence since",
-                   "Property", "Age", "Other installment plans", "Housing", "Number of existing credits at this bank", "Job",
-                   "Number of people being liable to provide maintenance for", "Telephone", "foreign worker")
+german <- read_table("data/statlog+german+credit+data/german.csv", col_names = FALSE)
+
+names(german) <- c("Status_existing_checking_account", "Duration_month", "Credit_history", "Purpose", "amount",
+                   "Savings_account_bonds", "Present_employment_since", "Installment_rate_percentage_disposable_income",
+                   "Personal_status_and_sex", "Other_debtors_guarantors", "Present_residence_since",
+                   "Property", "Age", "Other_installment_plans", "Housing", "existing_credits", "Job",
+                   "Number_people_maintenance", "Telephone", "foreign_worker", "good")
+
+german_cats_dummies <- model.matrix(~ Status_existing_checking_account + Credit_history + 
+                                      Purpose + Savings_account_bonds + Present_employment_since + 
+                                      Personal_status_and_sex + Other_debtors_guarantors + Property + 
+                                      Other_installment_plans + Housing + Job + Telephone + 
+                                      foreign_worker, german) 
+
+german_with_dummies <- cbind(as.data.frame(german), as.data.frame(german_cats_dummies)) %>%
+  select(-`(Intercept)`, -Status_existing_checking_account, -Credit_history, 
+         -Purpose, -Savings_account_bonds, -Present_employment_since, 
+         -Personal_status_and_sex, -Other_debtors_guarantors, -Property,
+         -Other_installment_plans, -Housing, -Job, -Telephone, -foreign_worker)
+
+# change 1,2 to 0,1
+german_with_dummies$good <- german_with_dummies$good-1
+x <- german_with_dummies %>% 
+  select(-good) %>%
+  as.matrix()
+y <- german_with_dummies %>% 
+  select(good) %>%
+  as.matrix()
+
+gold <- german_with_dummies
