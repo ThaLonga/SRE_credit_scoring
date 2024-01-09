bc091fd343f414fd9beaf690ddc361cb65afb9d3
ThaLonga <simondelange2@gmail.com>
2024-01-09 09:36:28 +0100

    bug fix

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 # main
-
+if (!require("pacman")) install.packages("pacman") ; require("pacman")
+p_load(glmnet, glmnetUtils, mgcv, tidyverse, xgboost, DiagrammeR, stringr, tictoc, parallel, pROC, earth, Matrix, pre, caret, parsnip, ggplot2)
 #conventions:
 # target variable = label
 # indepentent variables = all others
@@ -11,11 +12,12 @@ source("./src/hyperparameters.R")
 source("./src/BigSummary.R")
 source("./src/data_loader.R")
 datasets <- load_data()
-
+cl <- makeCluster(detectCores()-1)
 #(AUCROC, Brier, partialGini)
-metric = "partialGini"
+metric = "AUCROC"
 nr_repeats = 5
 outerfolds = 2
+dataset_vector = c("GC", "AC", "GMSC")
 
 
 # create empty dataframe metric_results with columns: (dataset, repeat, fold, algorithm, metric)	
@@ -29,8 +31,10 @@ metric_results <- data.frame(
 )
 #metric_results[nrow(metric_results) + 1,] = list(dataset, 2, 1, "test", 40.485)
 
+dataset_counter = 0
 for(dataset in datasets) {
-  
+  dataset_counter <- dataset_counter + 1
+  if(dataset_counter==3) {nr_repeats <- 3}
   for(N in 1:nr_repeats) {
     print(paste("repeat",N))
     
@@ -42,20 +46,25 @@ for(dataset in datasets) {
     for(fold in 1:outerfolds) {
       print(paste("fold",fold))
       #create seed
-      innerseed <- N+10*fold
+      innerseed <- N*10+fold
       
       #select train and test
       train <- train_sets[[fold]]
       test <- test_sets[[fold]]
-      LRR_preds <- LR_R(train, test, seed = innerseed)
+      LRR_preds <- LR_R(train, test, n_folds = nr_repeats, seed = innerseed)
       LRR_preds$label <- test$label
-      g <- roc(label ~ X1, data = LRR_preds)
+      g <- roc(label ~ X1, data = LRR_preds, directory = "<")
       AUC <- g$auc
+      metric_results[nrow(metric_results) + 1,] = list(dataset_vector[dataset_counter], N, fold, "LRR", AUC)
       print(AUC)
+      
     }
 
   }
 }
+
+stopCluster(cl)
+
 
 set.seed(123)
 train_indices <- sample(1:nrow(x), 0.8 * nrow(x))
--- src/BigSummary.R
+++ src/BigSummary.R
@@ -10,18 +10,26 @@ BigSummary <- function (data, lev = NULL, model = NULL) {
   sorted_indices <- order(probs, decreasing = TRUE)
   sorted_probs <- probs[sorted_indices]
   sorted_actuals <- actuals[sorted_indices]
-  
+
   # Select subset with PD < 0.4
   subset_indices <- which(sorted_probs < 0.4)
   subset_probs <- sorted_probs[subset_indices]
   subset_actuals <- sorted_actuals[subset_indices]
-  
-  # Calculate ROC curve for the subset
-  roc_subset <- pROC::roc(subset_actuals, subset_probs, quiet = TRUE)
-  # Calculate AUC for the subset
-  partial_auc <- pROC::auc(roc_subset)
-  # Calculate partial Gini coefficient
-  partial_gini <- 2 * partial_auc - 1
+
+  # Check if there are both positive and negative cases in the subset
+  if (length(unique(subset_actuals)) > 1) {
+    # Calculate ROC curve for the subset
+    roc_subset <- pROC::roc(subset_actuals, subset_probs,
+                            direction = "<", quiet = TRUE)
+    # Calculate AUC for the subset
+    partial_auc <- pROC::auc(roc_subset)
+    # Calculate partial Gini coefficient
+    partial_gini <- 2 * partial_auc - 1
+  } else {
+    # Set partial Gini to NA if there are not enough cases for ROC calculation
+    partial_gini <- NA
+  }
+    
   
   if (inherits(brscore, "try-error")) brscore <- NA
   rocAUC <- if (inherits(rocObject, "try-error")) {
