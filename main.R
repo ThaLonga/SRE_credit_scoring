06506ab7160f620c3a2bf5efc5d4bf063c0bd768
ThaLonga <simondelange2@gmail.com>
2024-01-10 17:32:34 +0100

    add gam

--- main.R
+++ main.R
@@ -41,23 +41,26 @@ dataset_counter = 1
 
 for(dataset in datasets) {
   
-  #formulate recipes
+  # formulate recipes
+    # for tree-based algorithms
   TREE_recipe <- recipe(label ~., data = dataset) %>%
     step_impute_mean(all_numeric_predictors()) %>%
     step_impute_mode(all_string_predictors()) %>%
     step_impute_mode(all_factor_predictors())
   
+    # for algorithms using linear terms (LRR, gam, rule ensembles)
   original_numeric_predictors <- names(dataset)[sapply(dataset, is.numeric)]
   LINEAR_recipe <- TREE_recipe %>%
     step_hai_winsorized_truncate(all_numeric_predictors(), fraction = 0.025) %>%
-    step_select(-original_numeric_predictors)%>%
+    step_select(-all_of(original_numeric_predictors))%>%
     step_normalize(all_numeric_predictors()) %>%
     step_dummy(all_string_predictors()) %>%
-    step_dummy(all_factor_predictors())
+    step_dummy(all_factor_predictors()) %>%
+    step_zv(all_predictors())
+
   
-  
-  if(dataset_counter==3) {nr_repeats <- 3}
-  else {nr_repeats <- 5}
+  # for GMSC only 3 repeats because large dataset
+  if(dataset_counter==3) {nr_repeats <- 3} else {nr_repeats <- 5}
   
   set.seed(123)
   # create 5x2 folds
@@ -73,12 +76,13 @@ for(dataset in datasets) {
     # LRR
     #####
     
-    LRR_model <- train(LINEAR_recipe, data = train,  method = "glmnet", trControl = ctrl, metric = metric,
+    LRR_model <- train(LINEAR_recipe, data = train,  method = "glmnet", trControl = ctrl, metric = "partialGini",
                        tuneGrid = expand.grid(alpha = hyperparameters_LR_R$alpha,lambda = hyperparameters_LR_R$lambda),
                        allowParallel=TRUE)
     LRR_preds <- predict(LRR_model, test, type = 'probs')
     print("pred ok")
     LRR_preds$label <- test$label
+    #AUC
     g <- roc(label ~ X1, data = LRR_preds, direction = "<")
     AUC <- g$auc
     metric_results[nrow(metric_results) + 1,] = list(dataset_vector[dataset_counter], i, "LRR", AUC)
@@ -86,6 +90,35 @@ for(dataset in datasets) {
     
     #####
     # GAM
+    #####
+    
+    #preprocessing as no hyperparameters to be tuned
+    train_bake <- LINEAR_recipe %>% prep(train) %>% bake(train)
+    test_bake <- LINEAR_recipe %>% prep(train) %>% bake(test)
+    train_bake_x <- train_bake %>% select(-label)
+    
+    smooth_vars = colnames(train_bake_x)[get_splineworthy_columns(train_bake_x)]
+    formula <- as.formula(
+      stringr::str_sub(paste("label ~", 
+                             paste(ifelse(names(train_bake_x) %in% smooth_vars, "s(", ""),
+                                   names(train_bake_x),
+                                   ifelse(names(train_bake_x) %in% smooth_vars, ")",""),
+                                   collapse = " + ")
+      ), 0, -1)
+    )
+    
+    GAM_model <- gam(formula, family = "binomial", data = train_bake)
+    GAM_preds <- data.frame(X1 = as.vector(predict(GAM_model, test_bake, type = 'response')))
+    print("pred ok")
+    GAM_preds$label <- test_bake$label
+    #AUC
+    g <- roc(label ~ X1, data = GAM_preds, direction = "<")
+    AUC <- g$auc
+    metric_results[nrow(metric_results) + 1,] = list(dataset_vector[dataset_counter], i, "GAM", AUC)
+    print(AUC)
+    
+    #####
+    # CTREE
     #####
     
     
